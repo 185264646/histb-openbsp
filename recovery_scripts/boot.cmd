62637acb91c61938215502cf3236d02e9b3ae534
notfound405 <43387518+185264646@users.noreply.github.com>
2023-04-12 20:19:33 +0000

    recovery_scripts: use another mechanism for continue sysboot

--- recovery_scripts/boot.cmd
+++ recovery_scripts/boot.cmd
@@ -1,6 +1,7 @@
 # set autostart
 env set autostart yes
-env set bootmenu_0 Continue sysboot=sysboot usb 0:1
+# overwrite default bootmeth seq and try bootflow again
+env set bootmenu_0 Continue sysboot=bootmeth order "pxe syslinux script"; bootflow scan -lb
 env set bootmenu_1 Load l-loader.bin from USB and flash it to eMMC=usb reset; fatload usb 0:1 $loadaddr l-loader.bin && mmc write $loadaddr 0 800; reset
 
 bootmenu 30
