c64e108d83a00845c6f2aed931afc22c9de44362
notfound405 <43387518+185264646@users.noreply.github.com>
2023-04-16 13:20:31 +0800

    recovery_scripts: select mmc dev 1 explicitely to avoid writing to SD Card

--- recovery_scripts/boot.cmd
+++ recovery_scripts/boot.cmd
@@ -1,7 +1,7 @@
 # set autostart
 env set autostart yes
 # overwrite default bootmeth seq and try bootflow again
-env set bootmenu_0 Continue sysboot=bootmeth order "pxe syslinux script"; bootflow scan -lb
-env set bootmenu_1 Load l-loader.bin from USB and flash it to eMMC=usb reset; fatload usb 0:1 $loadaddr l-loader.bin && mmc write $loadaddr 0 800; reset
+env set bootmenu_0 'Continue sysboot=bootmeth order "distro script"; bootflow scan -lb'
+env set bootmenu_1 'Load l-loader.bin from USB and flash it to eMMC=mmc dev 1; usb reset; fatload usb 0:1 $loadaddr l-loader.bin && mmc write $loadaddr 0 800; reset'
 
 bootmenu 30
