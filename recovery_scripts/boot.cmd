48ee4b51929e6e3a46c4a6aff98e91c75abdb90c
notfound405 <43387518+185264646@users.noreply.github.com>
2023-04-22 03:33:15 +0800

    recovery_scripts: update comment

--- recovery_scripts/boot.cmd
+++ recovery_scripts/boot.cmd
@@ -1,6 +1,9 @@
 # set autostart
 env set autostart yes
-# overwrite default bootmeth seq and try bootflow again
+
+# originally it searches for boot.scr first, so we are always sourced before
+# extlinux.conf. We override it here and retry a bootflow scan. A better idea
+# is welcomed.
 env set bootmenu_0 'Continue sysboot=bootmeth order "distro script"; bootflow scan -lb'
 env set bootmenu_1 'Load l-loader.bin from USB and flash it to eMMC=mmc dev 1; usb reset; fatload usb 0:1 $loadaddr l-loader.bin && mmc write $loadaddr 0 800; reset'
 
