735e69634df3ebb64d45fbc91ea6887bf5e7c40a
notfound405 <43387518+185264646@users.noreply.github.com>
2023-04-22 03:33:53 +0800

    recovery_scripts: skip MBR when flashing l-loader.bin

--- recovery_scripts/boot.cmd
+++ recovery_scripts/boot.cmd
@@ -1,10 +1,16 @@
 # set autostart
 env set autostart yes
 
+# set loadaddr
+env set loadaddr 0x1000000
+env set loadaddr_skip_mbr 0x1000200
+
 # originally it searches for boot.scr first, so we are always sourced before
 # extlinux.conf. We override it here and retry a bootflow scan. A better idea
 # is welcomed.
 env set bootmenu_0 'Continue sysboot=bootmeth order "distro script"; bootflow scan -lb'
-env set bootmenu_1 'Load l-loader.bin from USB and flash it to eMMC=mmc dev 1; usb reset; fatload usb 0:1 $loadaddr l-loader.bin && mmc write $loadaddr 0 800; reset'
+
+# omit 1 sector to avoid overwritting MBR
+env set bootmenu_1 'Load l-loader.bin from USB and flash it to eMMC=mmc dev 1; usb reset; fatload usb 0:1 $loadaddr l-loader.bin && mmc write $loadaddr_skip_mbr 1 3FF; reset'
 
 bootmenu 30
